9bd58339caa89c0d83159dc53f0b4f121cbfbb33
Kuleen Sasse <cometku@gmail.com>
2023-03-30 20:01:26 -0400

    stealing rs485 code

--- acquisition/acquisition.ino
+++ acquisition/acquisition.ino
@@ -1,79 +1,49 @@
-#include <RingBuf.h>
+#include "RS485_protocol.h"
 
-void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
-  Serial.print("Acquisiton starting.");
+const byte ENABLE_PIN = 4;
+
+void fWrite(const byte what){
+  Serial.write(what);  
 }
-
-
-//RingBuf<uint8_t, 6> buf;
-
-#define SIG 0x53
-uint8_t buffer[7];
-int pos;
-
-enum state_t {IDLE, RECEIVING, READY};
-
-
-state_t state = IDLE;
-
-unsigned long time_start = 0;
-int period = 100;
-
-void loop() {
-  // if we get a read_request
-  //receivechar();
-  switch(state) {
-    case RECEIVING: {
-      while(Serial1.available()) {
-          buffer[pos] = Serial1.read();
-          if(buffer[0] != SIG) {
-            pos = 0;            
-            continue; // keep going till we get a packet starting with the signature
-          }
-          pos++;
-          if(pos > 7) {
-            pos = 0;
-            if(buffer[0] == SIG) {
-              time_start = millis();
-              state = READY;
-            }
-              
-            
-            break;
-
-          }
-        }
-    };
-    case READY: {
-      for(int i = 1; i < 6; i++) {
-        Serial.write(buffer[i]);
-        delay(1);
-      }
-      Serial.println();
-      memset(buffer, 0, 7); // clear the buffer for now
-      state = IDLE;
-    };
-    case IDLE: {
-      if(millis() > time_start + period) {
-        //Serial1.flush();
-        state = RECEIVING;
-      }
-    };
-  }
-}
-
-
-
-void receivechar() {
-  digitalWrite(3, LOW); // transmit on 
-  digitalWrite(2, HIGH); // receive off
-  int i = 0;
-
   
-  //Serial1.readBytes(buffer, 6);
-  //buf.push(Serial1.read());
-
-  //Serial1.flush();
+int fAvailable(){
+  return Serial.available();  
 }
+
+int fRead(){
+  return Serial.read();  
+}
+  
+void setup(){
+  Serial.begin (28800);
+  pinMode (ENABLE_PIN, OUTPUT);  // driver output enable
+}
+
+void loop(){
+  byte buf [10];
+  
+  byte received = recvMsg (fAvailable, fRead, buf, sizeof (buf));
+  
+  if (received){
+    if (buf [0] != 1)
+      return;  // not my device
+      
+    if (buf [1] != 2)
+      return;  // unknown command
+    
+    byte msg [] = {
+       0,  // device 0 (master)
+       3,  // turn light on command received
+    };
+    
+    delay (1);  // give the master a moment to prepare to receive
+    digitalWrite (ENABLE_PIN, HIGH);  // enable sending
+    sendMsg (fWrite, msg, sizeof msg);
+
+    while (!(UCSR0A & (1 << UDRE0)))  // Wait for empty transmit buffer
+    UCSR0A |= 1 << TXC0;  // mark transmission not complete
+    while (!(UCSR0A & (1 << TXC0)));   // Wait for the transmission to complete
+
+    digitalWrite (ENABLE_PIN, LOW);  // disable sending
+  }  // end if something received
+}  // end of loop
