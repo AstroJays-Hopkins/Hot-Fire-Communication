63c14db831b2b90076b60468d1eef9409fd96561
Kuleen Sasse <56900136+KuleenS@users.noreply.github.com>
2023-04-27 21:46:13 -0400

    Update protocol.h

--- library/protocol/protocol.h
+++ library/protocol/protocol.h
@@ -1,5 +1,9 @@
 typedef struct {
-    char header = 0x83;
-    int8_t type;
-    unsigned long counter;
+    char header = 0x83; //1
+    int8_t type; //1
+    unsigned long counter; //4
+    double tc1; //4
+    double tc2; //4
+    int pt1; //2
+    int pt2; //2
 } __attribute__((packed)) Response;
